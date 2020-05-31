7ad8fbda3913ba9aa29c11c5c6bf027c9722c4d9
PuneetShrivas <puneetshrivas32@gmail.com>
2020-05-31 16:05:22 +0530

    Contacts updated
    changes to config.js

--- content/hero/index.md
+++ content/hero/index.md
@@ -1,8 +1,8 @@
 ---
 title: 'Hi, my name is'
 name: 'Puneet Shrivas	'
-subtitle: 'I build things for tomorrow.'
+subtitle: 'I build tech for tomorrow.'
 buttonText: 'Get In Touch'
 ---
 
-I'm a Mechanical engineering undergraduate based in Mumbai, India. I actively develop and implement innovative technical projects 
+I'm a Mechanical Engineering Undergraduate at IIT Bombay. I am actively involved in developing and implementing innovative and sustainable technology projects 
--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Puneet Shrivas | IIT Bombay',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Puneet Shrivas\'s Portfolio',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Puneet Shrivas, Puneet, Shrivas, puneetshrivas32, mechanical engineer, cubesat engineer, space engineer, systems engineering, India',
+  siteUrl: 'https://puneetshrivas.netlify.app/',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Puneet Shrivas',
+  location: 'Mumbai, India',
+  email: 'puneetshrivas32@gmail.com',
+  github: 'https://github.com/PuneetShrivas',
+  twitterHandle: '@ShrivasPuneet',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/PuneetShrivas',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/puneet-shrivas/',
     },
     {
       name: 'Codepen',
@@ -28,11 +28,11 @@ module.exports = {
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/puneet.shrivas/',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/ShrivasPuneet',
     },
   ],
 
